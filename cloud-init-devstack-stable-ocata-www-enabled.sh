0067e3215508bd997258ecceb33d69bd3ac48c4e
James Benson <JamesOBenson@users.noreply.github.com>
2017-08-16 14:58:27 -0500

    Create cloud-init-devstack-stable-ocata-www-enabled.sh

---
+++ cloud-init-devstack-stable-ocata-www-enabled.sh
@@ -0,0 +1,36 @@
+#cloud-config
+
+users:
+  - default
+  - name: stack
+    lock_passwd: False
+    sudo: ["ALL=(ALL) NOPASSWD:ALL\nDefaults:stack !requiretty"]
+    shell: /bin/bash
+
+write_files:
+  - content: |
+        #!/bin/sh
+        echo 127.0.0.1 localhost $HOSTNAME >> /etc/hosts
+        DEBIAN_FRONTEND=noninteractive sudo apt-get -qqy update || sudo yum update -qy
+        DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy git || sudo yum install -qy git
+        sudo chown stack:stack /home/stack
+        cd /home/stack
+        git clone https://git.openstack.org/openstack-dev/devstack -b stable/ocata
+        cd devstack
+        echo '[[local|localrc]]' > local.conf
+        echo ADMIN_PASSWORD=password >> local.conf
+        echo DATABASE_PASSWORD=password >> local.conf
+        echo RABBIT_PASSWORD=password >> local.conf
+        echo SERVICE_PASSWORD=password >> local.conf
+        echo SERVICE_TOKEN=tokentoken >> local.conf
+        # The following 4 commands allow internet access to the instances
+        echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
+        echo net.ipv4.conf.default.rp_filter=0 >> /etc/sysctl.conf
+        echo net.ipv4.conf.all.rp_filter=0 >> /etc/sysctl.conf
+        sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+        ./stack.sh
+    path: /home/stack/start.sh
+    permissions: 0755
+
+runcmd:
+  - su -l stack ./start.sh
